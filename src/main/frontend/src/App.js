a9c455af7a2a608bb40b2a5328181122afce072e
kmanjt <kevin.tomescu2@mail.dcu.ie>
2022-06-10 15:35:42 +0100

    working axios GET with backend REST API

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -5,17 +5,25 @@ import React, {useState, useEffect} from "react";
 
 const UserProfiles = () => {
 
+    const [userProfiles, setUserProfiles] = useState([]);
+
     const fetchUserProfiles = () => {
       axios.get("http://localhost:8080/api/v1/user-profile").then(res => {
         console.log(res);
+        setUserProfiles(res.data);
       });
     }
     useEffect(() => {
       fetchUserProfiles();
     }, []);
 
-    return <h1>Hello World</h1>
-}
+    return userProfiles.map((userProfile, index) => {
+      return (<div>
+        <h1>{userProfile.username}</h1>
+        <p>{userProfile.userProfileId}</p>
+      </div>);
+    });
+};
 
 function App() {
   return (
